b17b8d79460db09e9ffe7e26e9de6c2beedcf14b
chrisalfonso <alfonso.chris@gmail.com>
2017-10-18 11:10:08 -0400

    Added cat levels

--- js/app.js
+++ js/app.js
@@ -4,25 +4,44 @@ var ViewModel = function() {
 	// There's a better way, but not 'til later.
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
-	//this.catLevel = ko.observable('Newborn')
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.imgAttribution = ko.observable('https://www.flickr.com');
 
-	// Probably need to look up this method.
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
-		if (clickCount < 5) {
-			this.catLevel = ko.observable('Newborn')
-		}
 	};
 
-	/* Cat Levels
+	// Cat Levels
 	this.catLevel = ko.computed(function() {
-		return "Kittens: " + this.incrementCounter();
+		switch(true) {
+			case this.clickCount() <= 5:
+				return "Newborn";
+				break;
+			case this.clickCount() <= 10:
+				return "Kitty";
+				break;
+			case this.clickCount() <= 20:
+				return "Madam/Sir Cat";
+				break;
+			case this.clickCount() <= 50:
+				return "Most Honorable Cat";
+				break;
+			case this.clickCount() > 50:
+				return "Immortal Cat";
+				break;
+			case this.clickCount() > 100:
+				return "Lord/Lordess of the Cats"
+		}
+		
 	}, this);
-	*/
 
+	
 }
+
+/*if (this.clickCount() <= 5) {
+			return "Newborn";
+		}
+		*/
 
 // Required
 ko.applyBindings(new ViewModel());
